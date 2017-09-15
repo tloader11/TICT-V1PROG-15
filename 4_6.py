d6074a2d6bac33158b103d0c2541543fef9d98d3
universal <tristan-floyd@hotmail.com>
2017-09-15 10:59:06 +0200

    v0.0.2

---
+++ 4_6.py
@@ -0,0 +1,12 @@
+def wijzig(letterlijst):
+    letterlijst.clear()
+    letterlijst.append('d')
+    letterlijst.append('e')
+    letterlijst.append('f')
+
+
+
+lijst = ['a', 'b', 'c']
+print(lijst)
+wijzig(lijst)
+print(lijst)
