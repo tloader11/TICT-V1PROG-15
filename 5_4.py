ad18a49ad7fd6bb0a00ee41b1d780b3996b6af42
universal <tristan-floyd@hotmail.com>
2017-09-20 15:43:32 +0200

    v1

---
+++ 5_4.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+while True:
+    file = open('hardlopers.txt', 'a') #opens for apending, creates file if not exist.
+    naam = input("Naam vd hardloper: ")
+    vandaag = datetime.datetime.today()
+    s = vandaag.strftime("%a %d %b %Y %H:%M:%S")
+    newfile = s + ", " + naam + '\n'
+    file.write(newfile)
+    file.flush()
+    file.close()
+    print(s, naam)
